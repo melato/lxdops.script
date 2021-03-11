d2cf2733139adf3e22967b503392c1408a6ffe12
Alex Athanasopoulos <alex@melato.org>
2021-03-11 22:13:48 +0100

    mariadb template, container

---
+++ debian/mariadb/mariadb.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+service mysql stop
+mv /var/lib/mysql/ib_logfile* /var/lib/mysql-log/
+cat >> /etc/mysql/my.cnf <<END
+[mysqld]
+skip-innodb_doublewrite
+innodb_use_native_aio=0
+innodb_use_atomic_writes=0
+
+innodb_log_group_home_dir = /var/lib/mysql-log
+bind-address		= 0.0.0.0
+END
+
+service mysql start
+sudo mkdir /var/opt/DB
